6ed3203d90a5cdb9b23403dafa8a62ac35d6053e
pr21 <pranav@aupplysail.com>
2025-12-01 19:03:00 -0700

    cleanup grafana

--- distributed-financial-db/db/init_cockroach.sh
+++ distributed-financial-db/db/init_cockroach.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
-set -e
+# #!/bin/bash
+# set -e
 
-echo "Waiting for CockroachDB node to be reachable..."
-until /cockroach/cockroach sql --insecure --host=cockroachdb-node1 -e 'SELECT 1'; do
-  echo "CockroachDB not ready yet... sleeping 3s"
-  sleep 3
-done
+# echo "Waiting for CockroachDB node to be reachable..."
+# until /cockroach/cockroach sql --insecure --host=cockroachdb-node1 -e 'SELECT 1'; do
+#   echo "CockroachDB not ready yet... sleeping 3s"
+#   sleep 3
+# done
 
-echo "CockroachDB is up. Creating database 'market'..."
-/cockroach/cockroach sql --insecure --host=cockroachdb-node1 -e "CREATE DATABASE IF NOT EXISTS market;"
+# echo "CockroachDB is up. Creating database 'market'..."
+# /cockroach/cockroach sql --insecure --host=cockroachdb-node1 -e "CREATE DATABASE IF NOT EXISTS market;"
 
-echo "Applying schema to 'market'..."
-cat /schema.sql | /cockroach/cockroach sql --insecure --host=cockroachdb-node1 -d market
+# echo "Applying schema to 'market'..."
+# cat /schema.sql | /cockroach/cockroach sql --insecure --host=cockroachdb-node1 -d market
 
-echo "Schema applied successfully."
+# echo "Schema applied successfully."
